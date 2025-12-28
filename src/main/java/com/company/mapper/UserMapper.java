254d4a6867719643395072ff162772b7e0842556
sgd <sgd@it.cn>
2025-12-28 22:15:10 +0800

    后端第三期用户模块-进行到userRegister接口

---
+++ src/main/java/com/company/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.company.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.company.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author gd
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
