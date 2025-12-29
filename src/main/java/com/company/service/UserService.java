4c89261a0e51972b267691708fa02fdb4e78022c
sgd <sgd@it.cn>
2025-12-30 00:42:25 +0800

    后端第三期用户模块-完成userRegister接口 正在开发userLogin

--- src/main/java/com/company/service/UserService.java
+++ src/main/java/com/company/service/UserService.java
@@ -1,5 +1,6 @@
 package com.company.service;
 
+import com.company.model.vo.LoginUserVO;
 import com.mybatisflex.core.service.IService;
 import com.company.model.entity.User;
 
@@ -10,5 +11,21 @@ import com.company.model.entity.User;
  */
 public interface UserService extends IService<User> {
 
-//    long userRegister(String username,String password,String checkPassword)
+    /**
+     * 用户注册
+     * @param userAccount
+     * @param password
+     * @param checkPassword
+     * @return
+     */
+    long userRegister(String userAccount,String password,String checkPassword);
+
+
+    /**
+     * 用户登录
+     * @param userAccount
+     * @param password
+     * @return
+     */
+    LoginUserVO userLogin(String userAccount,String password);
 }
